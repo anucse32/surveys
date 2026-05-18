f6d67afe408e5f3652b054cf94b4a98fae167376
Anu <anucse32@gmail.com>
2026-05-18 01:04:08 -0700

    Fixing lint issues.

--- app/controllers/responses_controller.rb
+++ app/controllers/responses_controller.rb
@@ -1,15 +1,15 @@
 class ResponsesController < ApplicationController
   ANSWER_MAPPING = { "yes" => true, "no" => false }.freeze
-  
+
   def create
     survey = Survey.find(params[:survey_id])
     answer = ANSWER_MAPPING[params[:answer].to_s.downcase]
-  
+
     if answer.nil?
       redirect_to survey_path(survey), alert: "Please choose Yes or No."
       return
     end
-  
+
     survey.responses.create!(answer: answer)
     redirect_to surveys_path, notice: "Response recorded."
   end
--- app/controllers/surveys_controller.rb
+++ app/controllers/surveys_controller.rb
@@ -2,11 +2,11 @@ class SurveysController < ApplicationController
   def index
       @surveys = Survey.with_response_stats.order(created_at: :desc)
   end
-  
+
   def new
     @survey = Survey.new
   end
-  
+
   def create
     @survey = Survey.new(survey_params)
     if @survey.save
@@ -15,13 +15,13 @@ class SurveysController < ApplicationController
       render :new, status: :unprocessable_content
     end
   end
-  
+
   def show
     @survey = Survey.find(params[:id])
   end
-  
+
   private
-  
+
   def survey_params
     params.require(:survey).permit(:question)
   end
